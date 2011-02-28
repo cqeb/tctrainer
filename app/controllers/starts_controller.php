dd23baa67f62e0270cdb510f2572d97e4658cf6b
Klaus-M. Schremser <klaus@tricoretraining.com>
2011-03-01 00:23:02 +0100

    new modal dialog for inviters and presenting workout data on startpage

--- app/controllers/starts_controller.php
+++ app/controllers/starts_controller.php
@@ -1,13 +1,14 @@
 <?php
 
-class StartsController extends AppController {
+class StartsController extends AppController 
+{
 	var $name = 'Starts';
 	// use no model
 	var $uses = null;
 	var $useTable = false;
 
-	var $helpers = array('Html', 'Form', 'Javascript', 'Time', 'Session'); // 'TabDisplay',
-	var $components = array('Cookie', 'RequestHandler', 'Session', 'Unitcalc', 'Filldatabase');
+	var $helpers = array('Html', 'Form', 'Javascript', 'Time', 'Session', 'Unitcalc'); // 'TabDisplay',
+	var $components = array('Cookie', 'RequestHandler', 'Session', 'Unitcalc', 'Filldatabase', 'Transactionhandler' );
 
 	function beforeFilter()
 	{
@@ -15,30 +16,85 @@ class StartsController extends AppController {
   		$this->layout = 'trainer_start';
 	}
 
-	function index()
+	function index( $language )
 	{
-
-      	if ( $this->Session->read('session_userid') )
-            $this->redirect('/trainingplans/view');
-      
       	$this->pageTitle = __('the interactive, online training plan service for run, bike and triathlon athletes ', true);
 
-      	if ( isset( $this->params['named'] ) ) 
+		if ( isset( $language ) )
+		{
+			if ( $language == 'de' ) $this->code = 'deu';
+			else $this->code = 'eng';
+			
+			$this->Session->write('Config.language', $this->code);
+		}
+		
+      	if ( isset( $this->params['named']['u'] ) ) 
       	{
-          if ( isset( $this->params['named']['distance'] ) ) $distance = $this->params['named']['distance'];
-          else $distance = '';
-          if ( isset( $this->params['named']['distance_unit'] ) ) $distance_unit = $this->params['named']['distance_unit'];
-          else $distance_unit = '';
-          if ( isset( $this->params['named']['duration'] ) ) $duration = $this->params['named']['duration'];
-          else $duration = '';
-          if ( isset( $this->params['named']['stype'] ) ) $stype = $this->params['named']['stype'];
-          else $stype = '';
-    
-          $this->set('distance', $distance);
-          $this->set('distance_unit', $distance_unit);
-          $this->set('duration', $duration);
-          $this->set('stype', $stype);
-      	}      
+      		$transaction_id = $this->params['named']['u'];
+			
+			// information is saved in transactions
+			$this->loadModel('Transaction');
+	
+			$transaction_content = $this->Transactionhandler->handle_transaction( $this->Transaction, $transaction_id, 'read' );
+    		$dt = unserialize($transaction_content['sm_recommend']);
+			
+			$distance = $dt['distance'];
+			$duration = $dt['duration'];
+			$distance_unit = $dt['distance_unit'];
+			$sport = $dt['sport'];
+			$userid = $dt['userid'];
+
+			// create title
+			$title = __('I did a', true) . ' ' . $distance . ' ' . $distance_unit . ' ' . 
+				__($sport . ' workout', true) . ' ' . __('in',true) . ' ' . $duration . ' ' . 
+				__('hour(s)',true) . ' ' . __('with', true) . ' ' . 'http://tricoretraining.com';
+				 
+			// read userinformation
+			$this->loadModel('User');
+			$results = $this->User->findById( $userid );
+						
+			$this->set('distance', $distance);
+			$this->set('distance_unit', $distance_unit);
+			$this->set('duration', $duration);
+			$this->set('sport', $sport);
+			$this->set('userinfo', $results['User']);
+			$this->set('title', $title);
+			
+			$this->Session->write('recommendation_userid', $results['User']['id']);
+					
+      	} elseif ( isset( $this->params['named']['ur'] ) ) 
+      	{
+ 			$userid = base64_decode( $this->params['named']['ur'] );
+			$this->loadModel('User');
+			$results = $this->User->findById( $userid );
+
+			$this->set('userinfo', $results['User']);
+			$this->Session->write('recommendation_userid', $results['User']['id']);
+			
+		// these users get money for new users
+      	} elseif ( isset( $this->params['named']['urm'] ) ) 
+      	{
+ 			$userid = base64_decode( $this->params['named']['urm'] );
+			$this->loadModel('User');
+			$results = $this->User->findById( $userid );
+
+			$this->set('userinfo', $results['User']);
+			$this->Session->write('recommendation_userid', 'money:' . $results['User']['id']);
+			
+ 		} elseif ( isset( $this->params['named']['c'] ) )
+ 		{
+ 			$company_email = base64_decode( $this->params['named']['c'] );
+			//echo base64_encode( '@gentics.com' );
+			if ( isset( $company_email ) )
+			{ 
+				$this->Session->write('recommendation_userid', $company_email);
+				$this->set( 'companyinfo', $company_email );
+			}
+			
+		} elseif ( $this->Session->read('session_userid') )
+            $this->redirect('/trainingplans/view');
+     
+
 	}
   
   function error404()
@@ -52,11 +108,36 @@ class StartsController extends AppController {
 	$this->set('statusbox', 'statusbox');
   }
 
+	function change_language()
+	{
+
+		if ( isset( $this->params['named']['code'] ) ) 
+			$this->code = $this->params['named']['code'];
+		else
+			$this->code = 'eng';
+
+	    if ( $this->Session->read('session_userid') )
+	    {
+	          $this->User->id = $this->Session->read('session_userid');
+	          $this->User->savefield('yourlanguage', $this->code, false);
+	    }
+
+		$this->Session->write('Config.language', $this->code);
+		$this->Session->setFlash(__('Language changed.',true));
+
+		if ( $this->referer() ) 
+		    $this->redirect($this->referer());
+	    else 
+	        $this->redirect(array('action'=>'index'));
+
+	}
+
   function fill_my_database()
   {
       $this->checkSession();
 	  $userobject = $this->Session->read('userobject');
-      if ( isset( $userobject['admin'] ) && $userobject['advanced_features'] == 1 )
+	  
+      if ( isset( $userobject['admin'] ) )
 	  {
 		      $this->autoRender = false;            
 		      $this->Filldatabase->prefill($this->Start);
